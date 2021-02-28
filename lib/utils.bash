c51442c227b2c4b9f32dadbb5e5a5c4904d96706
vptitsyn <vasily.ptitsyn@indriver.com>
2021-02-28 13:07:31 +0900

    change lib/utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for swiftgen.
 GH_REPO="https://github.com/SwiftGen/SwiftGen"
 
 fail() {
@@ -12,7 +11,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if swiftgen is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -25,12 +23,10 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if swiftgen has other means of determining installable versions.
   list_github_tags
 }
 
@@ -39,8 +35,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for swiftgen
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/${version}/swiftgen-${version}.zip"
 
   echo "* Downloading swiftgen release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -55,15 +50,13 @@ install_version() {
     fail "asdf-swiftgen supports release installs only"
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
-  local release_file="$install_path/swiftgen-$version.tar.gz"
+  local release_file="$install_path/swiftgen-${version}.zip"
   (
     mkdir -p "$install_path"
     download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
+    unzip "$release_file" -d "$install_path" || fail "Could not extract $release_file"
     rm "$release_file"
 
-    # TODO: Asert swiftgen executable exists.
     local tool_cmd
     tool_cmd="$(echo "swiftgen --help" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
